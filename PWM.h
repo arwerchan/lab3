c8860112245deac31d3cc9b1e11191291f60c18d
arwerchan <arwerchan@email.arizona.edu>
2015-10-26 12:49:06 -0700

    Create PWM.h

---
+++ PWM.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   PWM.h
+ * Author: arwerchan
+ *
+ * Created on October 19, 2015, 11:24 AM
+ */
+
+#ifndef PWM_H
+#define	PWM_H
+
+void initPWM1();
+void initPWM2();
+void initPWM3();
+void initPWM4();
+
+#endif	/* PWM_H */
+
